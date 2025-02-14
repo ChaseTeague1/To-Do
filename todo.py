78cad7aaea74c1f6321b047324abaeae30d7f18f
Chase Teague <106894422+ChaseTeague1@users.noreply.github.com>
2025-02-14 12:24:38 -0500

    seperated entry field, buttons, and task list into respective frames/widgets as well as experimenting with styling

--- todo.py
+++ todo.py
@@ -2,31 +2,43 @@ import tkinter as tk
 from tkinter import messagebox
 
 root = tk.Tk()
-root.geometry("400x600")
+root.geometry("400x400")
+root.config(bg="#5b5b5b")
+
+#creating frames for organization
+frame_input = tk.Frame(root, bg="#5b5b5b")
+frame_input.pack(pady=10)
+
+frame_buttons = tk.Frame(root, bg="#5b5b5b")
+frame_buttons.pack(pady=10)
+
+frame_tasklist = tk.Frame(root, bg="#5b5b5b")
+frame_tasklist.pack(pady=10)
 
 #input box
-task_entry = tk.Entry(root, width=40)
+task_entry = tk.Entry(frame_input, width=40, bg="#fffae7")
 task_entry.pack(pady=10)
 
 #create buttons (add, delete, complete)
-add_button = tk.Button(root, text="Add Task", width=20, command=lambda: add_task())
+add_button = tk.Button(frame_buttons, text="Add Task", width=20, command=lambda: add_task())
 add_button.pack(pady=5)
 
-delete_button = tk.Button(root, text="Delete Task", width=20, command=lambda: delete_task())
+delete_button = tk.Button(frame_buttons, text="Delete Task", width=20, command=lambda: delete_task())
 delete_button.pack(pady=5)
 
-mark_button = tk.Button(root, text="Mark Completed", width=20, command=lambda: mark_complete())
+mark_button = tk.Button(frame_buttons, text="Mark Completed", width=20, command=lambda: mark_complete())
 mark_button.pack(pady=5)
 
 #display task
-task_listbox = tk.Listbox(root, width=40, height=10)
+task_listbox = tk.Listbox(frame_tasklist, width=40, height=10)
 task_listbox.pack(pady=10)
 
 #create add functionality
 def add_task():
     task = task_entry.get()
+    added_task = f"- {task}"
     if task != "":
-        task_listbox.insert(tk.END, task)
+        task_listbox.insert(tk.END, added_task)
         task_entry.delete(0, tk.END)
     else:
         messagebox.showwarning("Input Error", "Please enter a task")
