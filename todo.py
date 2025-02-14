3f5db5104765f1c76cc9844ad0aa3354dbea35f3
Chase Teague <106894422+ChaseTeague1@users.noreply.github.com>
2025-02-14 09:06:14 -0500

    created Initial buttons for adding, deleting and marking task complete

--- todo.py
+++ todo.py
@@ -9,15 +9,31 @@ task_entry = tk.Entry(root, width=40)
 task_entry.pack(pady=10)
 
 #create buttons (add, delete, complete)
+add_button = tk.Button(root, text="Add Task", width=20, command=lambda: add_task())
+add_button.pack(pady=5)
 
+delete_button = tk.Button(root, text="Delete Task", width=20, command=lambda: delete_task())
+delete_button.pack(pady=5)
+
+mark_button = tk.Button(root, text="Mark Completed", width=20, command=lambda: mark_complete())
+mark_button.pack(pady=5)
+
+#display task
+task_listbox = tk.Listbox(root, width=40, height=10)
+task_listbox.pack(pady=10)
 
 #create add functionality
-
+def add_task():
+    pass
 
 #create delete functionality
+def delete_task():
+    pass
 
 
 #create mark complete functionality
+def mark_complete():
+    pass
 
 
 
