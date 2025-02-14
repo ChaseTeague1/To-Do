33dae8ac8f6c0373a89b2ed312e9f4c98f784f56
Chase Teague <106894422+ChaseTeague1@users.noreply.github.com>
2025-02-14 08:59:39 -0500

    created the GUI and documented the basic steps / functionality needed for this program

---
+++ todo.py
@@ -0,0 +1,25 @@
+import tkinter as tk
+from tkinter import messagebox
+
+root = tk.Tk()
+root.geometry("400x600")
+
+#input box
+task_entry = tk.Entry(root, width=40)
+task_entry.pack(pady=10)
+
+#create buttons (add, delete, complete)
+
+
+#create add functionality
+
+
+#create delete functionality
+
+
+#create mark complete functionality
+
+
+
+root.title("To-Do List")
+root.mainloop()
